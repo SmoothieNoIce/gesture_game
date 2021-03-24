4f36d7c68cfcc07874e04aff00f2e433fd8c55ba
NTOU <fus22803281@gmail.com>
2021-03-24 15:22:20 +0800

    Initial Commit

---
+++ gesture_game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gesture_game
+//
+//  Created by User17 on 2021/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gesture_game/gesture_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  gesture_gameApp.swift
+//  gesture_game
+//
+//  Created by User17 on 2021/3/24.
+//
+
+import SwiftUI
+
+@main
+struct gesture_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
